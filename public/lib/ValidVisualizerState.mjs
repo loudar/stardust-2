567fbebf50bf2c3cb0cfa3d12fc6ca76d0dc1440
Alexander Fritsch <35202909+loudar@users.noreply.github.com>
2024-01-10 13:44:05 +0100

    add particles to enum

--- public/lib/ValidVisualizerState.mjs
+++ public/lib/ValidVisualizerState.mjs
@@ -1,4 +1,4 @@
 export class ValidVisualizerState {
-    static frameTypes = ["bars", "grid", "circle", "spiral", "flimmer"];
+    static frameTypes = ["bars", "grid", "circle", "spiral", "flimmer", "particles"];
     static renderTypes = ["2D", "3D"];
 }
